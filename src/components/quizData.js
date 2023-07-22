f7fccebaa8476a70308f55e457e019ad9ec49a5b
chandan kumar <chandankumarmech18@gmail.com>
2023-07-22 09:39:13 +0530

    first commit

---
+++ src/components/quizData.js
@@ -0,0 +1,26 @@
+export const quizData = [
+  {
+    id: 0,
+    question: ` FunctionUp ka Co-Founder kaun hai?`,
+    options: [`Bharat Gupta`, `Rahul Hadeu`, `Hariom Yadav`],
+    answer: `Bharat Gupta`,
+  },
+  {
+    id: 1,
+    question: `CSK ka caption kaun hai?`,
+    options: [`THALA`, `CHIKU`, `HITMAN`],
+    answer: `THALA`,
+  },
+  {
+    id: 3,
+    question: `Virat Kohli ne IPL me kitne Century lga diya hai?`,
+    options: [`9`, `7`, `5`],
+    answer: `7`,
+  },
+  {
+    id: 4,
+    question: `Rahul Sir Kis day Ko alg form me dikh rhe the?`,
+    options: [`Monday`, `Friday`, `Hame to nhi lga`],
+    answer: `Friday`,
+  },
+];
